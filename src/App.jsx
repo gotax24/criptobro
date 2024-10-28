ed344d4c44496e5b0adefa0e3f3923d25ae83e97
gotax24 <124403304+gotax24@users.noreply.github.com>
2024-10-28 18:36:25 -0500

    Cambio en como mostrar la lista y aplicar css para la lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import Cuadricula from "./cuadricula";
+import "./App.css";
 
 function App() {
   const API_URL = import.meta.env.VITE_API_URL;
 
   const [criptos, setCripto] = useState();
 
-  const api = async () => {
-    try {
-      const res = await axios.get(`${API_URL}assets`);
-      console.log(res.data.data)
-      const data = res.data;
-      setCripto(data.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
+    const api = async () => {
+      try {
+        const res = await axios.get(`${API_URL}assets`);
+        const data = res.data;
+        setCripto(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     api();
-  }, []);
+  }, [API_URL]);
 
   if (!criptos) return <span>Cargando...</span>;
 
   return (
     <>
-      <h1>Lista de criptomoneda</h1>
-      <ol>
-        {criptos.map(({ id, name, priceUsd }) => (
-          <li key={id}>
-            Nombre: {name} - Precio: {Number(priceUsd).toFixed(2)}$
-          </li>
-        ))}
-      </ol>
+      <h1>Lista de Criptomonedas</h1>
+      <div className="container">
+        {criptos.map(
+          ({ id, name, symbol, priceUsd, changePercent24Hr, rank }) => (
+            <Cuadricula
+              key={id}
+              rank={rank}
+              name={name}
+              symbol={symbol}
+              priceUsd={priceUsd}
+              change={Number(changePercent24Hr).toFixed(2)}
+            />
+          )
+        )}
+      </div>
     </>
   );
 }
 
 export default App;
+
+//Crear una cuadricula con las criptos moneda y ademas hacer que el li sea un componente para hacer la lista
