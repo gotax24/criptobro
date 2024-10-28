fba933e4020e79f09bbe91e1bbb40ade53d8fe38
gotax24 <124403304+gotax24@users.noreply.github.com>
2024-10-27 21:32:34 -0500

    Se creo la lista de cripto moneda se agrego axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const [criptos, setCripto] = useState();
+
+  const api = async () => {
+    try {
+      const res = await axios.get(`${API_URL}assets`);
+      console.log(res.data.data)
+      const data = res.data;
+      setCripto(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    api();
+  }, []);
+
+  if (!criptos) return <span>Cargando...</span>;
+
   return (
     <>
-     <h1>hola mundo</h1>
+      <h1>Lista de criptomoneda</h1>
+      <ol>
+        {criptos.map(({ id, name, priceUsd }) => (
+          <li key={id}>
+            Nombre: {name} - Precio: {Number(priceUsd).toFixed(2)}$
+          </li>
+        ))}
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
